23711d99c02c9e70387c93a8e95717af8c297e4a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-06-13 16:49:26 +0100

    Show how to use Araxis Merge instead of Beyond Compare

--- demo_approvals_and_googletest/main.cpp
+++ demo_approvals_and_googletest/main.cpp
@@ -1,2 +1,5 @@
 #define APPROVALS_GOOGLETEST
 #include "ApprovalTests.hpp"
+
+// Optionally, override the Reporter in all cases where code did not specify what to use:
+// auto defaultReportDisposer = Approvals::useAsDefaultReporter( std::make_shared<Windows::AraxisMergeReporter>() );
