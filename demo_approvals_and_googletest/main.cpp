2c025711a84f6fb9468cb62458fb8b21a1f61aba
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-06-13 20:03:18 +0100

    Move output files to a sub-directory, for Google Test demo

--- demo_approvals_and_googletest/main.cpp
+++ demo_approvals_and_googletest/main.cpp
@@ -1,5 +1,8 @@
 #define APPROVALS_GOOGLETEST
 #include "ApprovalTests.hpp"
 
+// Put output files in a sub-directory
+auto subDirectoryDisposer = Approvals::useApprovalsSubdirectory("approval_tests");
+
 // Optionally, override the Reporter in all cases where code did not specify what to use:
 // auto defaultReportDisposer = Approvals::useAsDefaultReporter( std::make_shared<Windows::AraxisMergeReporter>() );
